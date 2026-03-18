ba91e59beba40d86e70cf252a27e31dd412f703e
Heverton Luiz <heverton1818@hotmail.com>
2026-03-18 19:00:04 -0300

    Primeiro Commit do banco-api-tests

---
+++ test/login.test.js
@@ -0,0 +1,33 @@
+const request = require('supertest');
+const { expect } = require('chai');
+
+describe('Login', () => {
+  describe('POST /login', () => {
+    it('Deve retornar 200 com o token em string quando usar credenciais válidas',
+      async () => {
+        const response = await request('http://localhost:3000')
+          .post('/login')
+          .set('Content-Type', 'application/json')
+          .send({ username: 'julio.lima', senha: '123456' })
+
+
+        expect(response.status).to.equal(200);
+        expect(response.body.token).to.be.a('string');
+      });
+  });
+
+  describe('POST /login', () => {
+    it('Deve retornar 400 com mensagem de erro quando não enviar as credenciais',
+      async () => {
+        const response = await request('http://localhost:3000')
+          .post('/login')
+          .set('Content-Type', 'application/json')
+          .send({ username: '', senha: '' })
+
+
+        expect(response.status).to.equal(400);
+        expect(response.body.error).to.equal('Usuário e senha são obrigatórios.');
+      });
+  });
+
+});
