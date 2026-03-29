05cda704ffc933262b25db7239eba224a7fe17be
Heverton Luiz <heverton1818@hotmail.com>
2026-03-28 22:07:17 -0300

    Adicionando testes de transferencia

--- test/login.test.js
+++ test/login.test.js
@@ -8,8 +8,10 @@ describe('Login', () => {
         const response = await request('http://localhost:3000')
           .post('/login')
           .set('Content-Type', 'application/json')
-          .send({ username: 'julio.lima', senha: '123456' })
-
+          .send({
+            username: 'julio.lima',
+            senha: '123456'
+          })
 
         expect(response.status).to.equal(200);
         expect(response.body.token).to.be.a('string');
