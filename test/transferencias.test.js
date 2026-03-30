906155bde0fce3bab2ed28903b1463149aff3e26
Heverton Luiz <heverton1818@hotmail.com>
2026-03-30 19:02:26 -0300

    Adicionei testes para get transferencia total e individual

--- test/transferencias.test.js
+++ test/transferencias.test.js
@@ -31,7 +31,7 @@ describe('Transferencias', () => {
     it('Deve retornar falha com 422 quando o valor da transferência for abaixo de R$ 10,00',
       async () => {
         const bodyTransferencias = { ...postTransferencias }
-        bodyTransferencias.valor = 9.99
+        bodyTransferencias.valor = 9.99;
         const resposta = await request(process.env.BASE_URL)
           .post('/transferencias')
           .set('Content-Type', 'application/json')
@@ -39,6 +39,44 @@ describe('Transferencias', () => {
           .send(bodyTransferencias);
         expect(resposta.status).to.equal(422);
       });
+  });
+
+  describe('GET /transferencias/{id}', () => {
+    it('Deve retornar sucesso com 200 e dados iguais ao registro de transferência contido no banco de dados quando o id for válido',
+      async () => {
+        const resposta = await request(process.env.BASE_URL)
+          .get('/transferencias/11')
+          .set('Authorization', 'Bearer ' + token)
+
+        const bodyEsperado = {
+          id: 11,
+          conta_origem_id: 1,
+          conta_destino_id: 2,
+          valor: '10.01',
+          data_hora: '2026-03-30T20:47:39.000Z',
+          autenticada: 0
+        }
+
+        expect(resposta.status).to.equal(200);
+        expect(resposta.body).to.deep.equal(bodyEsperado);
+
+      });
+  });
+
+  describe('GET /transferencias', () => {
+    it('Deve retornar 10 elementos na paginação quando informar limite de 10 registros',
+      async () => {
+        const resposta = await request(process.env.BASE_URL)
+          .get('/transferencias?page=1&limit=10')
+          .set('Authorization', 'Bearer ' + token)
+
+
+        expect(resposta.status).to.equal(200);
+        expect(resposta.body.limit).to.equal(10);
+        expect(resposta.body.transferencias.length).to.equal(10);
+
+      });
+
   });
 
 });
